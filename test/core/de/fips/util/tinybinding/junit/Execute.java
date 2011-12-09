e54cd56abbbc8a0f46702c623647935e01f8693e
peichhorn <peichhor@web.de>
2011-12-09 23:14:42 +0100

    cleanup

--- test/core/de/fips/util/tinybinding/junit/Execute.java
+++ test/core/de/fips/util/tinybinding/junit/Execute.java
@@ -1,8 +1,28 @@
+/*
+ * Copyright © 2010-2011 Philipp Eichhorn.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package de.fips.util.tinybinding.junit;
 
 import static org.fest.swing.edt.GuiActionRunner.execute;
 
-import java.awt.Component;
 import java.lang.reflect.Method;
 
 import lombok.AccessLevel;
@@ -18,8 +38,8 @@ import org.mockito.internal.creation.jmock.ClassImposterizer;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public final class Execute {
 	@RequiredArgsConstructor
-	private static class MethodInterceptorImpl<TYPE extends Component> implements MethodInterceptor {
-		private final TYPE component;
+	private static class MethodInterceptorImpl<TYPE> implements MethodInterceptor {
+		private final TYPE object;
 
 		@Override
 		public Object intercept(final Object obj, final Method method, final Object[] args, final MethodProxy proxy) throws Throwable {
@@ -27,14 +47,14 @@ public final class Execute {
 				@Override
 				@SneakyThrows
 				protected Object executeInEDT() {
-					return method.invoke(component, args);
+					return method.invoke(object, args);
 				}
 			});
 		}
 	}
 
-	public static <TYPE extends Component> TYPE inEDT(final TYPE component) {
-		final MethodInterceptor callback = new MethodInterceptorImpl<TYPE>(component);
-		return Cast.uncheckedCast(ClassImposterizer.INSTANCE.imposterise(callback, component.getClass(), new Class<?>[0]));
+	public static <TYPE> TYPE inEDT(final TYPE object) {
+		final MethodInterceptor callback = new MethodInterceptorImpl<TYPE>(object);
+		return Cast.uncheckedCast(ClassImposterizer.INSTANCE.imposterise(callback, object.getClass(), new Class<?>[0]));
 	}
 }
